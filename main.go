2fb8b3f7d978f70948c47369deca39453ee7c111
hikobend <hiko.bkend@gmail.com>
2022-09-17 19:48:01 +0900

    todo delete

--- main.go
+++ main.go
@@ -62,7 +62,10 @@ func main() {
 	// 	fmt.Println(v)
 	// }
 
-	t, _ := models.GetTodo(1)
-	t.Content = "Update Todo"
-	t.UpdateTodo()
+	// t, _ := models.GetTodo(1)
+	// t.Content = "Update Todo"
+	// t.UpdateTodo()
+
+	t, _ := models.GetTodo(4)
+	t.DeleteTodo()
 }
