c9090db9e19cc65083ee9facc53dbb2138734a32
hikobend <hiko.bkend@gmail.com>
2022-09-18 17:05:25 +0900

    メモ　コード変更

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -5,6 +5,7 @@ import (
 )
 
 // func top(w http.ResponseWriter, r *http.Request) {
+// layout topを実行
 func top(w http.ResponseWriter, _ *http.Request) {
 	generateHTML(w, "Hello", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -7,12 +7,15 @@ import (
 	"net/http"
 )
 
+// 関数を作成して共通化
 func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
 	var files []string
+	// ファイル格納
 	for _, file := range filenames {
 		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
 	}
 
+	// templatesを事前にキャッシュしておいて、効率的に処理
 	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", data)
 }
